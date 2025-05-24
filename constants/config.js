682260afe230e756c150452301c22a219c7fa632
katerin <katerinjohanabaezflorez@gmail.com>
2025-05-23 23:03:19 -0500

    refactor: Actualización en config,js

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
 export const AUTH0_DOMAIN = "eventsbga.us.auth0.com";
 export const AUTH0_CLIENT_ID = "91dOXcXA8e1UToIQq8ArVy4jtuN4Yssn";
 export const BACKEND_URL = "http://192.168.1.7:5000";
-export const REDIRECT_URI = 'exp://192.168.1.7:8081';
+export const REDIRECT_URI = "myapp://auth";
